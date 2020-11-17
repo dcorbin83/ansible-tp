83d15049bc430a221fa7d6c1d706e2eab04945dc
kubecorn <kubecorn@localhost.localdomain>
2020-11-17 10:53:29 -0500

    improve ssh script

--- ansible/sshSetup.sh
+++ ansible/sshSetup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-ssh-keygen -R $1
-ssh-copy-id kubecorn@$1
+var=$(docker container inspect $(docker container ps -aq) | egrep "IPAddress" | grep [0-9] | awk '{ print $2 }')
 
+for i in $var
+do
+tmp=${i::-1}
+address="${tmp//\"}"
+ssh-keygen -R $address
+ssh-copy-id kubecorn@$address
+done
 
